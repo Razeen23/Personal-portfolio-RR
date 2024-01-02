caacc0bdccb5db118ef1e0b2ad8ac0874f792868
Razeen <makerrazeen2301@gmail.com>
2024-01-02 23:20:09 +0530

    first commit
    nav bar done

---
+++ src/components/Nav.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { BiHomeAlt, BiUser, BiSolidBriefcaseAlt2, BiCommentCheck } from 'react-icons/bi';
+import { BsFillClipboardDataFill } from 'react-icons/bs';
+import { Link } from 'react-scroll';
+
+const Nav = () => {
+  return (
+    <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
+      <div className='container mx-auto'>
+        <div className='w-full bg-black/20 h-[60px] backdrop-blur-2x1 rounded-full max-w-[460px] mx-auto px-5 flex items-center justify-between text-2x1 text-white/50'>
+          <Link activeClass='active'
+          smooth={true}
+          spy={true}
+          offset={-200}
+           to='home' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+            <BiHomeAlt />
+          </Link>
+          <Link activeClass='active'
+          smooth={true}
+          spy={true}
+           to='about' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+            <BiUser />
+          </Link>
+          <Link activeClass='active'
+          smooth={true}
+          spy={true}
+           to='services' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+            <BsFillClipboardDataFill />
+          </Link>
+          <Link activeClass='active'
+          smooth={true}
+          spy={true}
+           to='work' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+            <BiSolidBriefcaseAlt2 />
+          </Link>
+          <Link activeClass='active'
+          smooth={true}
+          spy={true}
+           to='contact' className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
+            <BiCommentCheck />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
